93f8fe8f8e966f16f9b057e6fde232359307fb1f
이승환 <seoung2460@gmail.com>
2026-02-28 03:14:59 +0900

    백엔드 환경 세팅 - DB주소 등록 및 DB접속이 가능한 아이디, 비번 입력 필요

---
+++ src/main/java/com/locker/be/LockerBeApplication.java
@@ -0,0 +1,13 @@
+package com.locker.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerBeApplication.class, args);
+	}
+
+}

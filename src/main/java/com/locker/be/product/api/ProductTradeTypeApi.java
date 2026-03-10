9bbdff6b59d4b663de5af611309377892a9cb6bf
LeeSeoungHwan <162408326+LeeSeoungHwan@users.noreply.github.com>
2026-03-10 09:18:04 +0900

    ResponseStatus 추가
    ResponseStatus 추가

--- src/main/java/com/locker/be/product/api/ProductTradeTypeApi.java
+++ src/main/java/com/locker/be/product/api/ProductTradeTypeApi.java
@@ -20,6 +20,7 @@ public class ProductTradeTypeApi {
     private final ProductTradeTypeService productTradeTypeService;
 
     @PutMapping("/create")
+    @ResponseStatus(value = HttpStatus.OK)
     @Operation(summary = "신규 데이터를 입력합니다.")
     public void findAll(ProductTradeTypeDto.ProductTradeTypeReq dto) {
         productTradeTypeService.create(dto);

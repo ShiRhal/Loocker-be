a755b1204bf9aecd0431ab17623adbed43453690
LeeSeoungHwan <162408326+LeeSeoungHwan@users.noreply.github.com>
2026-03-10 09:23:02 +0900

    import 수정
    import 수정

--- src/main/java/com/locker/be/product/api/ProductTradeTypeApi.java
+++ src/main/java/com/locker/be/product/api/ProductTradeTypeApi.java
@@ -6,9 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController

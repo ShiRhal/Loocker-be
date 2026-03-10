1dee88eadf3099d41ac3776e85b0f1178edbbadb
LeeSeoungHwan <162408326+LeeSeoungHwan@users.noreply.github.com>
2026-03-10 09:28:59 +0900

    @RequestBody final 추가
    @RequestBody final 추가

--- src/main/java/com/locker/be/product/api/ProductTradeTypeApi.java
+++ src/main/java/com/locker/be/product/api/ProductTradeTypeApi.java
@@ -20,7 +20,7 @@ public class ProductTradeTypeApi {
     @PutMapping("/create")
     @ResponseStatus(value = HttpStatus.OK)
     @Operation(summary = "신규 데이터를 입력합니다.")
-    public void create(ProductTradeTypeDto.ProductTradeTypeReq dto) {
+    public void create(@RequestBody final ProductTradeTypeDto.ProductTradeTypeReq dto) {
         productTradeTypeService.create(dto);
     }
 }

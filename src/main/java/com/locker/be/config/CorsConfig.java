5f5a1adafc799a26555acc570e4ee80963de79f0
Seungmin <harry5843@naver.com>
2026-03-25 20:31:39 +0900

    CORS Error
    CORS 에러 해결

--- src/main/java/com/locker/be/config/CorsConfig.java
+++ src/main/java/com/locker/be/config/CorsConfig.java
@@ -15,7 +15,11 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+        ));
+    
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setExposedHeaders(List.of("Authorization"));

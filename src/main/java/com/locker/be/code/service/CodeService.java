1c849dc7a539dcebbdde0eaa2b6d62e878c25e31
이승환 <seoung2460@gmail.com>
2026-04-05 17:01:56 +0900

    코드관련 조회 api 생성

---
+++ src/main/java/com/locker/be/code/service/CodeService.java
@@ -0,0 +1,42 @@
+package com.locker.be.code.service;
+
+import com.locker.be.code.mapper.CodeMapper;
+import com.locker.be.code.dto.CodeQueryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CodeService {
+
+    private final CodeMapper codeMapper;
+
+    // @Transactional(readOnly = true)
+    public Collection<CodeQueryDto.StateRes> findState() {
+
+        return codeMapper.findState();
+    }
+
+    // @Transactional(readOnly = true)
+    public Collection<CodeQueryDto.CityRes> findCity() {
+
+        return codeMapper.findCity();
+    }
+
+    // @Transactional(readOnly = true)
+    public Collection<CodeQueryDto.MainCategoryRes> findMainCategory() {
+
+        return codeMapper.findMainCategory();
+    }
+
+    // @Transactional(readOnly = true)
+    public Collection<CodeQueryDto.SubCategoryRes> findSubCategory() {
+
+        return codeMapper.findSubCategory();
+    }
+
+}

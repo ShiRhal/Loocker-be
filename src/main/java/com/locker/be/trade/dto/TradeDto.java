2433b73d1ac28b32891160c54aa929b01e622d8c
이승환 <seoung2460@gmail.com>
2026-04-17 20:19:47 +0900

    반환 ID 수정

--- src/main/java/com/locker/be/trade/dto/TradeDto.java
+++ src/main/java/com/locker/be/trade/dto/TradeDto.java
@@ -14,19 +14,19 @@ public class TradeDto {
         private Long PRODUCT_ID;
         private Long USER_ID;
         private String TRADE_TYPE_CODE;
-        private Long TRADE_ID;
+        private Long CHAT_ROOM_ID;
 
         @Builder
         public TradeInsertReq (
                 Long PRODUCT_ID,
                 Long USER_ID,
                 String TRADE_TYPE_CODE,
-                Long TRADE_ID
+                Long CHAT_ROOM_ID
         ) {
             this.PRODUCT_ID = PRODUCT_ID;
             this.USER_ID = USER_ID;
             this.TRADE_TYPE_CODE = TRADE_TYPE_CODE;
-            this.TRADE_ID = TRADE_ID;
+            this.CHAT_ROOM_ID = CHAT_ROOM_ID;
         }
     }
 }
